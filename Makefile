2116ea3727cbc94b50242d2b4187f41c780ed1e7
franciscoidalgo <fidalgo@frba.utn.edu.ar>
2021-08-10 23:40:17 -0300

    Template de tp SO con makefiles y script para deploy

---
+++ Makefile
@@ -0,0 +1,5 @@
+all:
+	make -C modulo1
+
+clean:
+	make clean -C modulo1
---
+++ deploy.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+UTNSO_LIBRARIES=("so-commons-library" "so-nivel-gui-library")
+CWD=$PWD
+MODULOS=("modulo1")
+for UTNSO_LIB in ${UTNSO_LIBRARIES[@]}
+do
+    echo -e "\n\nInstalling ${UTNSO_LIB}\n\n"
+    git clone "https://github.com/sisoputnfrba/${UTNSO_LIB}.git" #|| echo -e "\nLibrary is already cloned"
+    cd $UTNSO_LIB
+    sudo make uninstall
+    sudo make all
+    sudo make install
+    cd $CWD
+    sudo rm -f -r $UTNSO_LIB
+    echo -e "\n\nDone!!!"
+#    if ! cat .gitignore | grep -q "${UTNSO_LIB}/*"
+#    then
+#        echo -e "\nAdded ${UTNSO_LIB} to .gitignore"
+#        echo -e "\n${UTNSO_LIB}/*" | cat >> .gitignore
+#    fi
+done
+
+for MODULO in ${MODULOS[@]}
+do
+    echo -e "\n\nBuilding ${MODULO}\n\n"
+    make -C $MODULO
+    echo -e "\n\nDone!!!\n\n"
+done
